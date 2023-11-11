f79dc0c00194638427fb6d53a9e1b3633fb227f2
leticia8 <leticialado@gmail.com>
2023-11-11 11:48:22 +0100

    Add confi

--- backend/app/student_residences/settings.py
+++ backend/app/student_residences/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import os
 import environ
 import datetime
+import boto3
+from decouple import config
 
 # Initialise environment variables
 env = environ.Env()
@@ -24,9 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+#session = boto3.Session(region_name='eu-west-1')
+#ssm = session.client('ssm')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("AWS_ACCES_KEY_ID")
+SECRET_KEY = config("AWS_ACCES_KEY_ID")
+#hostname = ssm.get_parameter(Name=SECRET_KEY, WithDecryption=True)
+
 
 ENVIRONMENT = env("ENVIRONMENT")
 
