8c53b8f5371515f1fd6342300d8bd45b9c5c86e6
leticia8 <leticialado@gmail.com>
2023-11-11 21:33:53 +0100

    Adjustments

--- backend/app/student_residences/settings.py
+++ backend/app/student_residences/settings.py
@@ -21,7 +21,7 @@ env = environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ssm = boto3.client('ssm', 'sa-east-1')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
